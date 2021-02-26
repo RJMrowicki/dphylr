f9dead3dba9cadf134fbd3ca257eff50ebc1586c
Rob Mrowicki <robert.mrowicki@gmail.com>
2021-02-26 16:01:33 +0000

    enable species delimitation matrix in `plot_tree()`

--- R/plot_tree.R
+++ R/plot_tree.R
@@ -1,6 +1,7 @@
 #' Plot a tree
 #'
-#' Plot a labelled and formatted phylogenetic tree.
+#' Plot a labelled and formatted phylogenetic tree, optionally with a matrix
+#' summarising the results of species delimitation analyses.
 #'
 #' @param tree_obj A phylogenetic tree, in `ape` phylo format.
 #' @param og A character vector comprising the names of one or more sequences
@@ -18,13 +19,18 @@
 #'   if plotted.
 #' @param lht Line height (in cm) for tree tips (and the species delimitation
 #'   heatmap, if plotted).
+#' @param plot_delim Should corresponding species delimitation results be
+#'   plotted as a group association matrix to the right of the tree?
+#' @param spp_delim A data frame containing the combined species-sample
+#'   association results from one or more delimitation analyses, as output by
+#'   `comb_delim()`.
 #' @return A `ggtree` plot object, including associated data table.
 #' @examples
 #' plot_tree()
 #' @export
 plot_tree <- function (
   tree_obj, og, shorten_og, node_vals_lab, samp_seq_lab,
-  xext = 0.5, lht = 0.5
+  xext = 0.5, lht = 0.5, plot_delim = FALSE, spp_delim
 ) {
   p0 <-  # initiate the plot and assign to object
     # (NB -- reduce line thickness; plot ladderised tree in correct orientation)
@@ -64,7 +70,8 @@ plot_tree <- function (
 
   # add combined ML boot and BI pp node labels to plot data:
   p0$data %<>% dplyr::left_join(
-    dplyr::select(node_vals_lab, node, nodelab), by = "node")
+    dplyr::select(node_vals_lab, node, nodelab), by = "node"
+  )
 
 
   # extract current x limits:
@@ -97,19 +104,58 @@ plot_tree <- function (
         subset = grp == 0, label = tiplab),  # group0 samples
       parse = TRUE, align = FALSE, size = 3, colour = "black") +
 
-    geom_nodelab(  # add customised node labels
+    ggtree::geom_nodelab(  # add customised node labels
       aes(label = nodelab),  # use 'x = branch' for label on branch
       hjust = 1.11, vjust = -0.5, size = 2.5)
-    # ggrepel::geom_text_repel(  # add non-overlapping node labels
-    #   ggplot2::aes(label = nodelab),  # use 'x = branch' for label on branch
-    #   na.rm = TRUE,
-    #   box.padding = 0.01,  # default 0.25
-    #   # # for `ggrepel::geom_label_repel()`:
-    #   # label.padding = 0.1, label.size = 0, fill = adjustcolor("white", alpha = 0.75),
-    #   nudge_x = -0.005*xdist0,  # nudge_y = 0.0025*ydist0,
-    #   direction = "y", hjust = 0.25, vjust = -0.25, segment.size = 0.1, size = 2.5)
+  # ggrepel::geom_text_repel(  # add non-overlapping node labels
+  #   ggplot2::aes(label = nodelab),  # use 'x = branch' for label on branch
+  #   na.rm = TRUE,
+  #   box.padding = 0.01,  # default 0.25
+  #   # # for `ggrepel::geom_label_repel()`:
+  #   # label.padding = 0.1, label.size = 0, fill = adjustcolor("white", alpha = 0.75),
+  #   nudge_x = -0.005*xdist0,  # nudge_y = 0.0025*ydist0,
+  #   direction = "y", hjust = 0.25, vjust = -0.25, segment.size = 0.1, size = 2.5)
 
 
-  p +  # plot tree only
-    ggplot2::theme(plot.margin = margin(t = lht/2, unit = "cm"))
+  if (plot_delim) {  # if plotting a species delimitation matrix (heatmap),
+    # extract group association variables from delimitation results table and
+    # **convert `seq_code` col to rownames** (required by `ggtree::gheatmap()`)
+    sp_grps <- spp_delim %>% tibble::column_to_rownames("seq_code")
+    # extract overall number of levels (should be the same for all variables,
+    # representing the maximum number of groups delimited by any analysis):
+    n_sp_grps <- length(levels(dplyr::pull(sp_grps, 1)))
+
+    sp_colours <-  # create custom colour palette for heatmap
+      colorRampPalette(  # define custom colour sequence
+        rep(  # 2 colours, alternating between each adjacent group
+          c(grey(0.45), grey(0.85)), times = ceiling( n_sp_grps / 2 )
+        )[1:n_sp_grps],
+        alpha = TRUE, bias = 1  # return alpha values, alter colour spread
+      )
+
+    # specify total width of species matrix *relative* to tree width:
+    hm_width <- (lht/20) * (1 + xext) * ncol(sp_grps)
+    # (desired single column width [cm; = lht] / plot width [cm] *
+    # tree width [1] + x extension width [relative to tree width]) *
+    # no. of heatmap columns)
+
+    # specify species matrix offset in terms of *x scale*:
+    hm_offset <- xdist0*xext - (hm_width*xdist0)
+    # (max x limit - matrix width [converted to x units])
+
+    p %>%  # plot tree and associated species delimitation matrix
+      ggtree::gheatmap(
+        data = sp_grps,
+        width = hm_width, offset = hm_offset, color = "white",
+        colnames_angle = -45, hjust = 0, font.size = 3) +
+      ggplot2::scale_fill_manual(  # set matrix colours
+        # breaks = as.character(seq_len(n_sp_grps)),
+        breaks = ggplot2::waiver(),  # use default breaks (scale limits)
+        values = sp_colours(n_sp_grps),  # colour values from custom palette
+        guide = FALSE)
+
+  } else {
+    p +  # plot tree only
+      ggplot2::theme(plot.margin = margin(t = lht/2, unit = "cm"))
+  }
 }
